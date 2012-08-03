add666cc112f4476154263bc420ff32183c97140
Dave Fayram <dfayram@gmail.com>
2012-08-02 18:23:38 -0700

    Make the version real.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-ring-bootstrap "1.0.0-SNAPSHOT"
+(defproject clojure-ring-bootstrap "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [zookeeper-clj "0.9.1"]
