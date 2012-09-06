23f9165e1e04d6e5c2d2bfd03661493270b280a1
Matthew Gordon <matt.gordon@crowdflower.com>
2012-09-05 17:13:45 -0700

    We should not pull in an old Clojure version here!

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojure-ring-bootstrap "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [zookeeper-clj "0.9.1"]
+                 [zookeeper-clj "0.9.2" :exclusions [org.clojure/clojure]]
                  [clj-json "0.5.1"]
                  [cheshire "4.0.1"]
                  [metrics-clojure "0.8.0" :exclusions [cheshire]]
